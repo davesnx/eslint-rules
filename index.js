39a75d2daa63cf451fb661f2f6f51187cb88f23d
David Sancho <dsnxmoreno@gmail.com>
2019-05-15 12:34:11 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: [
+    'plugin:react/recommended',
+    'prettier/react',
+    'prettier-standard',
+    'standard',
+    'standard-jsx'
+  ],
+  plugins: ['filenames', 'react', 'prettier', 'react-hooks'],
+  settings: {
+    react: {
+      version: '16'
+    }
+  },
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      generators: true
+    }
+  },
+  globals: { fixture: true, FS: true },
+  rules: {
+    complexity: ['warn', { max: 7 }],
+    'filenames/match-regex': ['warn', '^_?[a-z0-9-.]+$'],
+    'prettier/prettier': ['error', { jsxSingleQuote: true }],
+    'react-hooks/rules-of-hooks': 'error',
+    "react/no-unsafe": 'warn',
+    'react/display-name': 'warn',
+    'react/prop-types': 'warn',
+    'react/jsx-no-bind': 0,
+    'react/jsx-handler-names': 'warn',
+    'curly': 'error',
+    'no-shadow': 'warn',
+    'no-else-return': 'warn',
+    'no-nested-ternary': 'warn',
+    'no-unneeded-ternary': 'warn',
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true
+      }
+    ]
+  }
+}
